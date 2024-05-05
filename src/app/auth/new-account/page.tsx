25b3422d69a1c32d0078a2ad6a4a65780bd46363
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2024-05-05 19:51:07 +0200

    Add new pages for shop admin, cart, category, checkout, empty, orders, product, and products

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function () {
   return (
     <main className="">
-      <h1>Hola mundo!!!</h1>
+      <h1>New Account</h1>
     </main>
   );
 }
